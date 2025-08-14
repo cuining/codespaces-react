3696f9ecbc685f2503d51749dfcc0b8e6b90aff1
cuining <cncc0824@gmail.com>
2025-08-14 14:26:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <p className="small">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <span>hello, whats yoru name?</span>
         <p>
           <a
             className="App-link"
